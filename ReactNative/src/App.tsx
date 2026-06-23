86e657d8a65ed8c398fc0b5bee96de30d8b74c3f
Yogendra Jangid <yogendra@x35innovationstudio.com>
2026-06-23 11:23:03 +0530

    [CAS-5471]update new design for example app

--- ReactNative/src/App.tsx
+++ ReactNative/src/App.tsx
@@ -1,20 +1,29 @@
-
-import React, { useEffect } from 'react';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import React, {useEffect} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import AppNavigation from './navigation/AppNavigation';
-import { StatusBar } from 'react-native';
-import { initialize } from 'contour-ai-sdk';
+import {StatusBar, StyleSheet} from 'react-native';
+import {initialize} from 'contour-ai-sdk';
 
 export default function App() {
-
   useEffect(() => {
     initialize('<CLIENT_ID>');
   }, []);
 
   return (
-   <SafeAreaView style={{ flex: 1 }} edges={['top', 'bottom']}>
-    <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
+    <SafeAreaView style={styles.safeArea} edges={['top', 'bottom']}>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="dark-content"
+      />
       <AppNavigation />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#d8e8ef',
+  },
+});
